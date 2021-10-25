e5b17be1e0a1716a01a77f1813fa07637b987236
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-25 21:23:37 +0200

    opdracht 2 af

--- gegevens.py
+++ gegevens.py
@@ -1,6 +1,7 @@
+# opdracht 1
 # feestlunch
-
 # gegevens
+
 AantalCroissantjes = 17
 CroissantPrijs = 0.39
 
@@ -11,3 +12,13 @@ AantalKortingsbonnen = 3
 KortingsbonWaarde = 0.50 
 
 print(AantalCroissantjes * CroissantPrijs + AantalStokbroden * StokbroodPrijs - AantalKortingsbonnen * KortingsbonWaarde)
+
+# opdracht 2
+# de speelhal-dag
+#gegevens
+Personen = 4
+Toegangsticket = 7.45
+GameSeat = 0.37
+GameSeatTijd = 5
+
+print(Personen * Toegangsticket + GameSeatTijd * 9 * 4 * GameSeat)
