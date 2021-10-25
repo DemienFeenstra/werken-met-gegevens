66e3ec2797e9f6894c0294824de3200d08926955
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-25 19:25:25 +0200

    opdracht 1 af

---
+++ gegevens.py
@@ -0,0 +1,13 @@
+# feestlunch
+
+# gegevens
+AantalCroissantjes = 17
+CroissantPrijs = 0.39
+
+AantalStokbroden = 2
+StokbroodPrijs = 2.78
+
+AantalKortingsbonnen = 3 
+KortingsbonWaarde = 0.50 
+
+print(AantalCroissantjes * CroissantPrijs + AantalStokbroden * StokbroodPrijs - AantalKortingsbonnen * KortingsbonWaarde)
