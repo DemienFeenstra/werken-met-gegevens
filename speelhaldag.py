b3e4e9559e0c41bc4bd11acef9666e3b977d5135
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-26 20:47:29 +0200

    screenshots en lab 3 klaar

--- speelhaldag.py
+++ speelhaldag.py
@@ -1,6 +1,9 @@
 # de speelhal-dag
 
-txt = "te betalen:"
+txt = "Dit geweldige dagje-uit met"
+txt2 = "mensen in de Speelhal met"
+txt3 = " minuten VR kost je maar"
+txt4 = "euro"
 
 Personen = 4
 Toegangsticket = 7.45
@@ -9,4 +12,4 @@ GameSeatTijd = 5
 
 totaal = Personen * Toegangsticket + GameSeatTijd * 9 * 4 * GameSeat
 
-print(txt, totaal)
+print(txt, Personen, txt2, GameSeatTijd * 9, txt3, totaal, txt4)
