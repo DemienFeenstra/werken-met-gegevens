6f7eba5ba6e6b0ed436599a8983dd116563e5e61
DemiënFeenstra <99064788@mydavinci.nl>
2021-10-26 14:20:42 +0200

    feestlunch lab2 af

---
+++ feestlunch.py
@@ -0,0 +1,17 @@
+# feestlunch
+
+txt = "het bedrag dat je moet betalen is:"
+
+AantalCroissantjes = 17
+CroissantPrijs = 0.39
+
+AantalStokbroden = 2
+StokbroodPrijs = 2.78
+
+AantalKortingsbonnen = 3 
+KortingsbonWaarde = 0.50 
+
+totaal = AantalCroissantjes * CroissantPrijs + AantalStokbroden * StokbroodPrijs - AantalKortingsbonnen * KortingsbonWaarde
+
+print(txt, totaal)
+ 
